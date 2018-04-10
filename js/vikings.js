031bf6ddf703257a7f11ca32595b37b26009d49d
AdriMan <adrimpartner@gmail.com>
2018-04-10 20:19:42 +0200

    saxons and vikings generators

--- js/vikings.js
+++ js/vikings.js
@@ -1,20 +1,40 @@
 // Vikings
-var Vikings = function(name, health, strength){
+var Viking = function(name, health, strength){
 	this.name = name;
-	this.health = health;
-	this.strength = strength;
+	this.health = Math.floor((Math.random() * 30 ) + 60);
+	this.strength = Math.floor((Math.random() * 20 ) + 10);
 }
 
 
 
 // Vikings Pit
 
+function getRandomTurn(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 var PitRing = function(vk1, vk2){
-	this.turns = 5;
+	this.turns = getRandomTurn(2,7);
 	this.vk1 = vk1;
 	this.vk2 = vk2;
-	
+
 };
+
+	var VikingRandom = Math.floor((Math.random() * 50) + 1);
+	var vikings = [];
+	var vikingsNumber = Math.floor((Math.random() * 80) + 10);
+	var vikingsDead = [];
+	// var name, health, strength;
+	// var viking;
+
+	for(var i= 0; i <= vikingsNumber; i++){
+		var name = 'vk'+i;
+		vikings.push(new Viking()); 
+		// vikings.push(Viking);
+
+	}
+	
+
 
 PitRing.prototype.fight = function(){
 		while(this.vk2.health > this.vk1.strength && this.vk1.health > this.vk2.strength ){
@@ -37,40 +57,67 @@ PitRing.prototype.fight = function(){
 			}
 		};
 
-var vk1 = new Vikings('Ragnar', 99, 20);
-var vk2 = new Vikings ('Rolo', 80, 15);
+var vk1 = new Viking('Ragnar', 99, 20);
+var vk2 = new Viking('Rolo', 80, 15);
 
 var pit = new PitRing(vk1, vk2);
 pit.fight();
 pit.winner();
 
+//SAXONS
 
+var Saxon = function(health, strength){
+	this.health = Math.floor((Math.random() * 40 ) + 10);
+	this.strength = Math.floor((Math.random() * 10 ) + 5);
+};
 
+var saxons = [];
+var SaxonsNumber = Math.floor((Math.random() * 80) + 10);
+var saxonsDead = []; 
 
-
-
-
-
-
-
-		
-
-
-
-
-
-// Saxons
-
-var Saxons = function(health, strength){
-	this.health = health;
-	this.strength = strength;
+for(var i = 0; i - SaxonsNumber; i++) {
+	saxons.push(new Saxon());
 }
 
 
-
-
-
-
 // Battle
+
+// var battle = function(VikingRandom, SaxonsRandom){
+// 	this.turns = Math.floor((Math.random() * 10) + 1);
+// 	this.VikingRandom = VikingRandom;
+// 	this.SaxonsRandom = SaxonsRandom;
+// };
+
+// var battleRing = new battle(vikings, saxons);
+// Viking = vikings;
+// Saxons = saxons;
+
+
+
+// battleRing.prototype.fight = function(){
+// 		while(this.Vikings.health > this.Saxons.strength && this.Saxons.health > this.Vikings.strength ){
+// 			this.Saxons.health -= this.Vikings.strength;
+// 			console.log('The viking ' + ' with the name of ' + this.Vikings.name + ' make a hit of '+ this.Vikings.strength + 
+// 				' and health of ' + this.Vikings.health);
+// 			this.Vikings.health -= this.Saxons.strength; 
+// 			console.log('The viking ' + ' with the name of ' + this.Saxons.name + ' make a hit of '+ this.Saxons.strength + 
+// 				' and health of ' + this.Saxons.health);
+
+// 			this.turns --;
+// 		}
+// 	};
+
+// battleRing.fight();
+
+
+
+
+
+
+
+
+
+
+
 
 
