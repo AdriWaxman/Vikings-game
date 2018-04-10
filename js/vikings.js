33647cbc3de4a80b1721c68740d26cd95d3dbd24
AdriMan <adrimpartner@gmail.com>
2018-04-10 21:11:20 +0200

    Battle init

--- js/vikings.js
+++ js/vikings.js
@@ -80,7 +80,49 @@ for(var i = 0; i - SaxonsNumber; i++) {
 }
 
 
-// Battle
+// BATTLE ROYALE
+
+var BattleRoyale = function(){
+	this.turn = getRandomTurn(3,8);
+	this.rounds = rounds; 
+}
+
+BattleRoyale.prototype.BattleArena = function(){
+	var initialVikings = this.vikings.length;
+	var initialSaxons = this.saxons.length;
+	// var fightTurn = 1; 
+	console.log('The vikings army are ' + initialVikings + ' soldiers'); 
+	console.log('The saxons army are ' + initialSaxons + ' possible dead people');
+	console.log(' The vikings army shout : VAMONOOOOOH!');
+	
+	while(vikings.length !== 0 && saxons.length !== 0){
+		if(this.vikings.health > 0 && this.saxons.health > 0){
+			this.saxons.health -= this.vikings.strength;
+		  this.vikings.health -= this.saxons.strength;
+		
+		console.log(this.vikings.name + ' causes ' + this.vikings.strength + ' damage points');
+		console.log(this.saxons.name + ' health is ' + this.saxons.health);
+		console.log(this.saxons.name + ' causes ' + this.saxons.strength + ' damage points');
+		console.log(this.vikings.name + ' health is ' + this.vikings.health);
+
+		turn --;
+		}
+
+		if(this.vikings.health === 0){
+			console.log('The viking dies!');
+			vikingsDead.push(this.vikings);
+			vikings = vikings.slice(1, vikings.length);
+			this.vikings = vikings[0]
+		}
+
+		if(this.saxons.health === 0){
+			console.log('The saxon dies!');
+			saxonsDead.push(this.saxons);
+			saxons = saxons.slice(1, saxons.length);
+			this.saxons = saxons[0]
+		}
+	};
+};
 
 // var battle = function(VikingRandom, SaxonsRandom){
 // 	this.turns = Math.floor((Math.random() * 10) + 1);
