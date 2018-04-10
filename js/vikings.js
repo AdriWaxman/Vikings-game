49ef476ff2c2654b28b18f7914e20edd4967f7bf
AdriMan <adrimpartner@gmail.com>
2018-04-10 14:06:33 +0200

    Pit created

--- js/vikings.js
+++ js/vikings.js
@@ -5,24 +5,41 @@ var Vikings = function(name, health, strength){
 	this.strength = strength;
 }
 
-var Ragnar = new Vikings('Ragnar', 99, 60)
-var Rolo = new Vikings ('Rolo', 80, 50)
+
+var vk1 = new Vikings('Ragnar', 99, 60)
+var vk2 = new Vikings ('Rolo', 80, 50)
 
 
 // Vikings Pit
 
-var Pit = {
+var Pit= function(vk1, vk2){
+	this.turns = turn;
+	this.player1 = vk1;
+	this.player2 = vk2;
+	this.fight = function(vk1, vk2, turns){
+		while(vk2.health > vk1.strength && vk1.health > vk2.strength ){
+			vk2.health -= vk1.strength;
+			vk1.health -= vk2.strength; 
+		}
+	};
+	winner : if(vk1.health < vk2.health){
+			console.log('The ' + vk2 + ' is he winner!!');
+		} else{
+			console.log('The ' + vk1 + ' is he winner!!')
+		}
+	
+};
 
-}
 
-var pitFight = function(viking1,viking2){
-	while(viking1.health >= 5 && viking2.health >= 5){
-		new viking2Health = viking2.health - viking1.strength;
-		new viking1Health = viking1.health - viking2.strength;
-	}
-	// return viking1.health - viking2.strength
-	// return viking2.health - viking1.strength
-}
+
+
+
+
+
+
+
+
+		
 
 
 
