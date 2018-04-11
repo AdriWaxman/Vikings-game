32f3fd4f8d5388268d19e2bd131f4cf20e162690
AdriMan <adrimpartner@gmail.com>
2018-04-11 14:09:21 +0200

    new Battle Arena

--- js/vikings.js
+++ js/vikings.js
@@ -21,15 +21,15 @@ var PitRing = function(vk1, vk2){
 };
 
 	var VikingRandom = Math.floor((Math.random() * 50) + 1);
-	var vikings = [];
+	var vikingsArmy = [];
 	var vikingsNumber = Math.floor((Math.random() * 80) + 10);
 	var vikingsDead = [];
 	// var name, health, strength;
 	// var viking;
 
 	for(var i= 0; i <= vikingsNumber; i++){
-		var name = 'vk'+i;
-		vikings.push(new Viking()); 
+		
+		vikingsArmy.push(new Viking()); 
 		// vikings.push(Viking);
 
 	}
@@ -61,8 +61,8 @@ var vk1 = new Viking('Ragnar', 99, 20);
 var vk2 = new Viking('Rolo', 80, 15);
 
 var pit = new PitRing(vk1, vk2);
-pit.fight();
-pit.winner();
+// pit.fight();
+// pit.winner();
 
 //SAXONS
 
@@ -71,85 +71,44 @@ var Saxon = function(health, strength){
 	this.strength = Math.floor((Math.random() * 10 ) + 5);
 };
 
-var saxons = [];
+var saxonsArmy = [];
 var SaxonsNumber = Math.floor((Math.random() * 80) + 10);
 var saxonsDead = []; 
 
 for(var i = 0; i - SaxonsNumber; i++) {
-	saxons.push(new Saxon());
+	saxonsArmy.push(new Saxon());
 }
 
 
 // BATTLE ROYALE
 
-var BattleRoyale = function(){
-	this.turn = getRandomTurn(3,8);
-	this.rounds = rounds; 
+var BattleRoyale = function(vikingArmy, saxonArmy){
+	this.rounds = getRandomTurn(3,8);
+	this.vikingArmy = vikingArmy;
+	this.saxonArmy = saxonArmy;
+	//this.rounds = rounds; 
 }
 
 BattleRoyale.prototype.BattleArena = function(){
-	var initialVikings = this.vikings.length;
-	var initialSaxons = this.saxons.length;
-	// var fightTurn = 1; 
-	console.log('The vikings army are ' + initialVikings + ' soldiers'); 
-	console.log('The saxons army are ' + initialSaxons + ' possible dead people');
-	console.log(' The vikings army shout : VAMONOOOOOH!');
-	
-	while(vikings.length !== 0 && saxons.length !== 0){
-		if(this.vikings.health > 0 && this.saxons.health > 0){
-			this.saxons.health -= this.vikings.strength;
-		  this.vikings.health -= this.saxons.strength;
-		
-		console.log(this.vikings.name + ' causes ' + this.vikings.strength + ' damage points');
-		console.log(this.saxons.name + ' health is ' + this.saxons.health);
-		console.log(this.saxons.name + ' causes ' + this.saxons.strength + ' damage points');
-		console.log(this.vikings.name + ' health is ' + this.vikings.health);
+	console.log('THE BATTLE BEGINS!!');
+	console.log('The vikings runs shouting : VAMONOOOH!');
 
-		turn --;
-		}
+	while(this.vikingArmy.health > this.saxonArmy.strength && this.saxonArmy.health > this.vikingArmy.strength){
+		this.vikingArmy.health -= this.saxonArmy.strength;
+		console.log('The saxons hit first!');
 
-		if(this.vikings.health === 0){
-			console.log('The viking dies!');
-			vikingsDead.push(this.vikings);
-			vikings = vikings.slice(1, vikings.length);
-			this.vikings = vikings[0]
-		}
+		this.saxonArmy.health -= this.vikingArmy.strength;
+		console.log('The vikings hit stronger!');
 
-		if(this.saxons.health === 0){
-			console.log('The saxon dies!');
-			saxonsDead.push(this.saxons);
-			saxons = saxons.slice(1, saxons.length);
-			this.saxons = saxons[0]
-		}
-	};
-};
+		this.rounds --;
+	}
+}
+// 	if(this.viking.health <= 0){
 
-// var battle = function(VikingRandom, SaxonsRandom){
-// 	this.turns = Math.floor((Math.random() * 10) + 1);
-// 	this.VikingRandom = VikingRandom;
-// 	this.SaxonsRandom = SaxonsRandom;
-// };
-
-// var battleRing = new battle(vikings, saxons);
-// Viking = vikings;
-// Saxons = saxons;
+// 	}
+// }
 
 
-
-// battleRing.prototype.fight = function(){
-// 		while(this.Vikings.health > this.Saxons.strength && this.Saxons.health > this.Vikings.strength ){
-// 			this.Saxons.health -= this.Vikings.strength;
-// 			console.log('The viking ' + ' with the name of ' + this.Vikings.name + ' make a hit of '+ this.Vikings.strength + 
-// 				' and health of ' + this.Vikings.health);
-// 			this.Vikings.health -= this.Saxons.strength; 
-// 			console.log('The viking ' + ' with the name of ' + this.Saxons.name + ' make a hit of '+ this.Saxons.strength + 
-// 				' and health of ' + this.Saxons.health);
-
-// 			this.turns --;
-// 		}
-// 	};
-
-// battleRing.fight();
 
 
 
